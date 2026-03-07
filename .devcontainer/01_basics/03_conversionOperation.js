99894ee70afb3a8b36b6bea45859f4603066883a
Ansh BDJ <anshbhardwaj3000@gmail.com>
2026-03-07 17:51:32 +0530

    prefix and postfix

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -22,6 +22,36 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
 
+//************ OPERATIONS ****************
+
+let value = 3
+let negvalue = -value
+//console.log(negvalue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**2);
+//console.log(2/2);
+//console.log(2%2);
+
+let str1 = "Ansh"
+let str2 = "BDJ"
+let str3 = str1 + str2
+//console.log(str3);
+
+//console.log("1" + 2);
+//console.log(1 + "2");
+//console.log("1" + 2 + 2);
+//console.log(1 + 2 + "2");
+//console.log((3+2)*5%3//)
+
+//console.log(+true);
+//console.log(+"");
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter);
