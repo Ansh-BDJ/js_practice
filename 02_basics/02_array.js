a3884b541baacd3fd6a16a2210fa8a9656a1215d
Ansh BDJ <anshbhardwaj3000@gmail.com>
2026-03-11 17:01:08 +0530

    02-arrays

---
+++ 02_basics/02_array.js
@@ -0,0 +1,26 @@
+const marvel_heros = ["thor", "IronMan", "spiderman"]
+const dc_heros = ["superman", "flash", "batman"]
+
+//marvel_heros.push(dc_heros)
+//console.log(marvel_heros);
+//console.log(marvel_heros[3][1]);
+
+const allHeros = marvel_heros.concat(dc_heros)
+//console.log(allHeros);
+
+
+const all_new_heros = [...marvel_heros, ...dc_heros] //spread operator
+//console.log(all_new_heros);
+
+const anotherArray = [1, 2, 3, 4, [5, 6], 7, [8, 9, [4, 5]]]
+//console.log(anotherArray.flat(Infinity));
+
+//console.log(Array.isArray("Ansh"));
+//console.log(Array.from("Ansh"));
+//console.log(Array.from({ name: "Ansh" })); //interesting
+
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+
+console.log(Array.of(score1, score2, score3));
